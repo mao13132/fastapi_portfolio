868183030b77b7f9075d9fdc4a9a5e9b0a98ecab
mao13132 <mao13132@gmail.com>
2024-12-15 20:31:58 +0300

    sorted works and add works

--- src/business/Works/worksAdmin.py
+++ src/business/Works/worksAdmin.py
@@ -14,8 +14,14 @@ from src.business.Works.works_table import Works
 class WorksAdmin(ModelView, model=Works):
     column_list = [work.name for work in Works.__table__.columns if work.name not in ['category']] + [Works.categories]
 
+    column_default_sort = [(Works.id, True)]
+
     name = 'Работа'
 
     name_plural = f'Работы'
 
     icon = 'fa-solid fa-layer-group'
+
+    page_size = 100
+
+    page_size_options = [25, 50, 100, 200]
